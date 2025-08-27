733ddd29bec8782ceb56cd85e97e5ebdedf2ed64
OwnParks <own.parks2025@gmail.com>
2025-08-27 13:24:44 +0530

    redeploy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,14 +15,14 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "OwnParks",
   description: "Future of Parking.",
-  icons: {
-    icon: [
-      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-      { url: "/favicon.ico", type: "image/x-icon" },
-    ],
-    apple: "/apple-touch-icon.png",
-  },
+icons: {
+  icon: [
+    { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+    { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    { url: "/favicon.ico", type: "image/x-icon" },
+  ],
+  apple: "/apple-touch-icon.png",
+},
 };
 
 export default function RootLayout({
